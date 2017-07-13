2746b0cf7e926c3c3992ee3dd7eca6794859c552
Maria Varga <marrieu@gmail.com>
2017-07-13 10:39:15 -0700

    Update creating_map.py
    reading from table

--- mapping/creating_map.py
+++ mapping/creating_map.py
@@ -1,10 +1,23 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+#data.columns
+lat = list(data['LAT'])
+lon = list(data['LON'])
 
 map = folium.Map(location=[38,-100], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My map")
-for cordinates in [[38,-100],[37,-90]]:
-    fg.add_child(folium.Marker(location=cordinates, popup="Hi, I am a marker", icon=folium.Icon(color="green")))
+
+#for cordinates in [[38,-100],[37,-90]]:
+    #fg.add_child(folium.Marker(location=cordinates, popup="Hi, I am a marker", icon=folium.Icon(color="green")))
+    
+for lt,ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Hi, I am a marker", icon=folium.Icon(color="green")))
+
+    
 map.add_child(fg)
 
 map.save("Map1.html")

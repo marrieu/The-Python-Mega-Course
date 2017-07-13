7e85e0dba5b52efd2e2eaf2ed10538e45d4cfbf9
Maria Varga <marrieu@gmail.com>
2017-07-12 19:53:59 -0700

    Update creating_map.py

--- mapping/creating_map.py
+++ mapping/creating_map.py
@@ -3,7 +3,8 @@ import folium
 map = folium.Map(location=[38,-100], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My map")
-fg.add_child(folium.Marker(location=[38,-100], popup="Hi, I am a marker", icon=folium.Icon(color="greenq")))
+for cordinates in [[38,-100],[37,-90]]:
+    fg.add_child(folium.Marker(location=cordinates, popup="Hi, I am a marker", icon=folium.Icon(color="green")))
 map.add_child(fg)
 
 map.save("Map1.html")

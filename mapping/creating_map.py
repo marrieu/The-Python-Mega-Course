4d16e3ec8aa2c68c55be15075cbd5b4cf46970a4
Maria Varga <marrieu@gmail.com>
2017-07-13 10:52:58 -0700

    Update creating_map.py
    adding elevation label

--- mapping/creating_map.py
+++ mapping/creating_map.py
@@ -6,6 +6,7 @@ data = pandas.read_csv("Volcanoes.txt")
 #data.columns
 lat = list(data['LAT'])
 lon = list(data['LON'])
+elev = list(data['ELEV'])
 
 map = folium.Map(location=[38,-100], zoom_start=6, tiles="Mapbox Bright")
 
@@ -14,8 +15,8 @@ fg = folium.FeatureGroup(name="My map")
 #for cordinates in [[38,-100],[37,-90]]:
     #fg.add_child(folium.Marker(location=cordinates, popup="Hi, I am a marker", icon=folium.Icon(color="green")))
     
-for lt,ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="Hi, I am a marker", icon=folium.Icon(color="green")))
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Elevation: "+str(el)+" m", icon=folium.Icon(color="green")))
 
     
 map.add_child(fg)

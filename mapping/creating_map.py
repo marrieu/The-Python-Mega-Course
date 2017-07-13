21052abeb9fd29ade0153d69d434725ba0642e7b
Maria Varga <marrieu@gmail.com>
2017-07-13 14:17:41 -0700

    Update creating_map.py
    adding color for different elevations

--- mapping/creating_map.py
+++ mapping/creating_map.py
@@ -8,6 +8,14 @@ lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return("green")
+    elif 1000 <= elevation <3000:
+        return("orange")
+    else:
+        return("red")
+    
 map = folium.Map(location=[38,-100], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My map")
@@ -16,7 +24,7 @@ fg = folium.FeatureGroup(name="My map")
     #fg.add_child(folium.Marker(location=cordinates, popup="Hi, I am a marker", icon=folium.Icon(color="green")))
     
 for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="Elevation: "+str(el)+" m", icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Elevation: "+str(el)+" m", icon=folium.Icon(color=color_producer(el))))
 
     
 map.add_child(fg)

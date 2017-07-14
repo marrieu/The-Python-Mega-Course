4440628db69a390e457b4d7aba3606e508902ade
Maria Varga <marrieu@gmail.com>
2017-07-14 15:45:23 -0700

    Create app3.py
    first commit

---
+++ app3-website-blocker/app3.py
@@ -0,0 +1,21 @@
+import time
+from datetime import datetime as dt
+
+hosts_path = "hosts.txt"
+redirect = "127.0.0.1"
+website_list=["www.facebook.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("test")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print("else")
+
+    time.sleep(3000)

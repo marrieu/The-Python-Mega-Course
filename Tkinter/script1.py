e8312329c43ffdb9ef1c0f3881a1341feb45fe30
Maria Varga <marrieu@gmail.com>
2017-07-30 20:07:43 -0700

    Update script1.py

--- Tkinter/script1.py
+++ Tkinter/script1.py
@@ -2,10 +2,22 @@ from tkinter import *
 
 window = Tk()
 
-b1 = Button(window, text="Execute")
+
+def km_to_mile():
+    # get because e1_value is not an actual string
+    print(e1_value.get())
+    miles = float(e1_value.get()) * 1.6
+    # print e1_value in the next shell(t1)
+    t1.insert(END, miles)
+
+
+b1 = Button(window, text="Execute", command=km_to_mile)
 b1.grid(row=0, column=0)
 
-e1 = Entry(window)
+# gets the value entered
+e1_value = StringVar()
+
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
 t1 = Text(window, height=1, width=20)
@@ -13,3 +25,4 @@ t1.grid(row=0, column=2)
 
 
 window.mainloop()
+

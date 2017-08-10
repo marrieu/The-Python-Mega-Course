5c5e1c847231e67bbdd00a60833ff7f79b2c3187
Maria Varga <marrieu@gmail.com>
2017-08-10 15:47:50 -0700

    Create scrip1.py

---
+++ SQLite-postgreSQL/scrip1.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE store (item TEXT,quantity INTEGER,price REAL)")
+    conn.commit()
+    conn.close()
+
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES(?,?,?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+
+insert("Water Glass", 10, 5)
+
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+print(view())
